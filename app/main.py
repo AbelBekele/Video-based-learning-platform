c550825389f009f0cc1d76e3deaf1c98f01fb4b0
Abel Bekele <abelbekele.addise@gmail.com>
2023-03-12 22:22:10 +0000

    Database session

--- app/main.py
+++ app/main.py
@@ -2,17 +2,18 @@ from fastapi import FastAPI
 
 from cassandra.cqlengine.management import sync_table
 
-from . import config, db
+from . import config, database
 from .users.models import User
 
 app = FastAPI()
+DB_SESSION = None
 settings = config.get_settings()
 
 @app.on_event("startup")
 def on_startup():
     # event is triggred when fastapi starts
-    print("hello world")
-    db.get_session()
+    print("Startup Sync")
+    DB_SESSION = database.get_session()
     sync_table(User)
 
 @app.get("/")
