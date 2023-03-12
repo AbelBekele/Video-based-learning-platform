2669084f70b2054bf3c5155f71256ee31611befc
Abel Bekele <abelbekele.addise@gmail.com>
2023-03-12 12:54:09 +0000

    Setting up startup event trigger

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 
-#from . import config
+from cassandra.cqlengine.management import sync_table
 
-main_app = FastAPI()
+from . import config
+
+app = FastAPI()
 #settings = config.get_settings()
 
-@main_app.get("/")
+@app.on_event("startup")
+def on_startup():
+    # event is triggred when fastapi starts
+    print("hello world")
+
+@app.get("/")
 def homepage() :
     return {"hello": "world", "keyspace": settings.keyspace} # json data -> REST API
---
+++ app/users/models.py
@@ -0,0 +1,19 @@
+import uuid
+
+from app.config import get_settings
+from cassandra.cassandra import columns
+from cassandra.cqlengine.models import Model
+
+setting = get_settings()
+
+class User(Model):
+    __keyspace__ = settings.keyspace
+    email = columns.Text(primary_key=True)
+    user_id = columns.UUID(primary_key=True, default=uuid.uuid1)
+    password = columns.Text()
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return f"User(email={self.email}, user_id={self.user_id})"
