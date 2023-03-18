8aeccb5ca2392446625ffa9883ed1b33cec8c5e1
Abel Bekele <abelbekele.addise@gmail.com>
2023-03-18 17:59:07 +0000

    Created pytest files

--- app/test_users.py
+++ app/test_users.py
@@ -1,7 +1,42 @@
+import pytest
+from app.users.models import User
+from app import database
 
+@pytest.fixture(scope='module')
+def setup():
+    # setting up test
+    session = database.get_session()
+    yield session
+    # after test finish tearingdown
+    queryset = User.objects.filter(email='bereded@gmail.com')
+    if queryset.count() != 0:
+        queryset.delete()
+    session.shutdown()
 
-def test_assert():
-    assert True is True
+def test_create_user(setup):
+    #session = setup
+    User.create_user(email ='bereded@gmail.com', password = 'bereded@vblp')
 
-def test_invalid_assert():
-    assert True is not True
+def test_duplicate_User(setup):
+    with pytest.raises(Exception):
+        User.create_user(email ='bereded@gmail.com', password = 'bereded@vblp')
+
+def test_invalid_email(setup):
+    with pytest.raises(Exception):
+        User.create_user(email='bereded@gmail', password='bereded@vblp')
+
+def test_valid_password(setup):
+    queryset = User.objects.filter(email='bereded@gmail.com')
+    assert queryset.count() == 1
+    user_obj = queryset.first()
+    assert user_obj.verify_password('bereded@vblp') == True
+    assert user_obj.verify_password('bereded@vbl') == False
+# def test_assert():
+#     assert True is True
+
+# def test_equal():
+#     assert 1 == 1
+
+# def test_invalid_assert():
+#     with pytest.raises(AssertionError):
+#         assert True is not True
