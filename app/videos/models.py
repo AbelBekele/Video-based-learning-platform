570b1f9c778aaa8b5030c82bcd53cb39280fbf5b
Abel Bekele <abelbekele.addise@gmail.com>
2023-03-21 09:55:21 +0000

    Creating schemas to handle exceptions well

--- app/videos/models.py
+++ app/videos/models.py
@@ -28,6 +28,10 @@ class Video(Model):
 
     def __repr__(self):
         return f"Video(host_id={self.host_id}, host_service={self.host_service})"
+    
+    def as_data(self):
+        return {f"{self.host_service}_id": self.host_id}
+
 
     @staticmethod
     def add_video(url, user_id=None):
