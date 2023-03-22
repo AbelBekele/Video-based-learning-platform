34b6211f5dac002e6d7edfc81a186c9f0847b77e
Abel Bekele <abelbekele.addise@gmail.com>
2023-03-22 09:37:32 +0000

    Created pydantic schema for watchevent

--- app/watchevents/pydantic_schemas.py
+++ app/watchevents/pydantic_schemas.py
@@ -1,5 +1,12 @@
+import uuid
 from pydantic import BaseModel
+from typing import Optional
 
 class WatchEventSchema(BaseModel):
     host_id: str
-    
+    #user_id: uuid.UUID
+    path: Optional[str]
+    start_time: float
+    end_time: float
+    duration: float
+    complete: bool
