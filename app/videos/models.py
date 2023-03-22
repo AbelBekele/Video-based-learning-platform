9aab96d6abed3c63e3f49ae3e9cfbdfb8699e949
Abel Bekele <abelbekele.addise@gmail.com>
2023-03-22 15:33:25 +0000

    Get or create method for adding videos

--- app/videos/models.py
+++ app/videos/models.py
@@ -5,6 +5,7 @@ from app.users.exceptions import InvalidUserIDException
 from app.shortcuts import templates
 from cassandra.cqlengine import columns
 from cassandra.cqlengine.models import Model
+from cassandra.cqlengine.query import (DoesNotExist, MultipleObjectsReturned)
 
 settings = get_settings()
 
@@ -44,6 +45,23 @@ class Video(Model):
     @property
     def path(self):
         return f"/videos/{self.host_id}"
+
+    @staticmethod
+    def get_or_create(url, user_id=None, **kwargs):
+        host_id = extract_video_id(url)
+        obj = None
+        created = False
+        try:
+            obj = Video.objects.get(host_id=host_id)
+        except MultipleObjectsReturned:
+            queryset = Video.objects.allow_filtering().filter(host_id=host_id)
+            obj = queryset.first()
+        except DoesNotExist:
+            obj = Video.add_video(url, user_id=user_id, **kwargs)
+            created = True
+        except:
+            raise Exception("Invalid Request")
+        return obj, created
 
     @staticmethod
     def add_video(url, user_id=None, **kwargs):
